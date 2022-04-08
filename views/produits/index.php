ef639e2bed59e3b90fc3f37dda75058000726842
Sandra <sandra.villot038@gmail.com>
2022-04-08 14:34:51 +0200

    affichage des images dans les views

--- views/produits/index.php
+++ views/produits/index.php
@@ -12,6 +12,11 @@ foreach($params['produits'] as $produit):
     <p>Date :<?= $produit->getCreatedAt() ?></p>
     <p>Description : <?= $produit->getDescription() ?></p>
     <p>Prix : <?= $produit->getPrix() ?></p>
+    <img src="public/pic/<?=$produit->getImage1() ?>" alt="" width="300px">
+
+    
+    
+    
 
     <a href="/Annonces/produits/<?= $produit->id ?>">Voir le produit</a>
     
