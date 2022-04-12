223cf5f0a070227bef131d2d810e0a9ec7c2f849
Eric S <shonga@msn.com>
2022-04-12 14:51:21 +0200

    formulaire presque fini

--- views/produits/form.php
+++ views/produits/form.php
@@ -1,23 +1,9 @@
 
-<h1>Insérer votre annonce</h1>
-<form action="/Annonces/formulaire" method="POST" enctype="multipart/form-data">
-    <div>
-        <label for="titre">Titre : </label>
-        <input type="text" id="titre" name="titre">
-    </div>
-    <div>
-        <label for="description">Description : </label>
-        <textarea name="description" id="description" cols="30" rows="10"></textarea>
-    </div>
-    <div>
-        <label for="prix">Prix : </label>
-        <input type="number" id="prix" name="prix">
-    </div>
-    <div>
-            <label for="image">Image</label>
-            <input type="file" accept = "image/jpg,image/jpeg,image/gif,image/png" name="image[]" id="img" multiple required>
-        </div>
-    <label for="Categorie">Categorie:</label>
+<h1>Créez votre annonce</h1>
+<div class="form_create">
+<div class="formulaire">
+
+<label for="Categorie">Categorie:</label><br>
     <select name="categorie" id="categorie">
         <option hidden>Liste des catégories</option>
         <option value="automobile">Auto</option>
@@ -26,10 +12,33 @@
         <option value="vetements">Vetements</option>
         <option value="multimedia">Multimedia</option>
         <option value="emploi">Emploi</option>
-    </select>
+    </select><br>
+    
+<form action="/Annonces/formulaire" method="POST" enctype="multipart/form-data">
+    <div>
+    <br><label for="titre">Titre : </label><br>
+        <input type="text" id="titre" name="titre">
+    </div>
+    <div>
+    <br><label for="description">Description : </label><br>
+        <textarea name="description" id="description" cols="30" rows="5"></textarea>
+    </div>
+    <div>
+    <br><label for="prix">Prix : </label><br>
+        <input type="number" id="prix" name="prix">
+    </div>
+    <div>
+    <br><label for="image">Image</label><br>
+            <input type="file" accept = "image/jpg,image/jpeg,image/gif,image/png" name="image[]" id="img" multiple required>
+        </div>
+        <br>
 
     <div id="submit">
         <input type="submit">
     </div>
-    <a href="/Annonces/">Retour</a>
+    
 </form>
+</div>
+</div>
+
+<a href="/Annonces/"><button class="seebtn">Retour</button></a>
--- views/produits/show.php
+++ views/produits/show.php
@@ -1,8 +1,11 @@
+<div class="show">
+<div class="infos_show">
 <h2>Titre : <?= $params['produit']->getTitre() ?></h2>
 <p>Categorie : <?= $params['produit']->getCategorie() ?></p>
 <p>Date : <?= $params['produit']->getCreatedAt() ?></p>
 <p>Description : <?= $params['produit']->getDescription() ?></p>
 <p>Prix : <?= $params['produit']->getPrix() ?></p>
+</div>
 
 <div class="caroussel_container">
     <input type="radio" name="position" checked />
@@ -11,12 +14,13 @@
     <input type="radio" name="position" />
     <input type="radio" name="position" />
     <main id="carousel">
-        <div class="item"><img src="../public/pic/<?= $params['produit']->getImage1() ?>" alt="" width="400px"></div>
-        <div class="item"><img src="../public/pic/<?= $params['produit']->getImage2() ?>" alt="" width="400px"></div>
-        <div class="item"><img src="../public/pic/<?= $params['produit']->getImage3() ?>" alt="" width="400px"></div>
-        <div class="item"><img src="../public/pic/<?= $params['produit']->getImage4() ?>" alt="" width="400px"></div>
-        <div class="item"><img src="../public/pic/<?= $params['produit']->getImage5() ?>" alt="" width="400px"></div>
+        <div class="item"><img src="../public/pic/<?= $params['produit']->getImage1() ?>" alt="" width="350px"></div>
+        <div class="item"><img src="../public/pic/<?= $params['produit']->getImage2() ?>" alt="" width="350px"></div>
+        <div class="item"><img src="../public/pic/<?= $params['produit']->getImage3() ?>" alt="" width="350px"></div>
+        <div class="item"><img src="../public/pic/<?= $params['produit']->getImage4() ?>" alt="" width="350px"></div>
+        <div class="item"><img src="../public/pic/<?= $params['produit']->getImage5() ?>" alt="" width="350px"></div>
         <main>
+</div>
 </div>
 
 
